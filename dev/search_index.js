70d647af5690c6cef9a80db0d58722c7f5102632
Documenter.jl <documenter@juliadocs.github.io>
2023-03-06 15:43:04 +0000

    build based on e4d7330

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Exdir","category":"page"},{"location":"#Exdir","page":"Home","title":"Exdir","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Exdir.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Exdir]","category":"page"}]
+}
