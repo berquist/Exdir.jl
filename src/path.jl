ebb1da1ddf14aa3df9d6b73f257e5b58a7398c91
Eric Berquist <eric.berquist@gmail.com>
2023-10-07 17:10:55 -0400

    Group: fix path formation

--- src/path.jl
+++ src/path.jl
@@ -1,5 +1,14 @@
+function clean_path(path::AbstractString)
+    path = normpath(path)
+    if isdirpath(path)
+        dirname(path)
+    else
+        path
+    end
+end
+
 function name_to_asserted_group_path(name::AbstractString)
-    path = name
+    path = clean_path(name)
     if isabspath(path)
         throw(ArgumentError("Absolute paths are currently not supported and unlikely to be implemented."))
     elseif splitpath(path) == [""]
@@ -9,11 +18,14 @@ function name_to_asserted_group_path(name::AbstractString)
 end
 
 function remove_root(path::AbstractString)
+    path = clean_path(path)
     components = splitpath(path)
-    rel = if components[1] == "/"
+    if components[1] == "/"
         joinpath(components[2:length(components)])
     else
         path
     end
-    rel
 end
+
+form_relative_path(parent_path::AbstractString, object_name::AbstractString) =
+    joinpath(parent_path, object_name) |> clean_path
