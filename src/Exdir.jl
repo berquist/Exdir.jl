580581badbb5491a549218f0dd9dcdc9a1544b25
Eric Berquist <eric.berquist@gmail.com>
2024-05-27 13:55:33 -0400

    start object file

--- src/Exdir.jl
+++ src/Exdir.jl
@@ -23,6 +23,7 @@ include("constants.jl")
 include("exceptions.jl")
 include("mode.jl")
 include("path.jl")
+include("object.jl")
 
 abstract type AbstractObject end
 abstract type AbstractGroup <: AbstractObject end
@@ -924,51 +925,6 @@ function require_dataset(grp::AbstractGroup, name::AbstractString;
     end
 
     current_object
-end
-
-function root_directory(path::AbstractString)
-    # https://github.com/CINPLA/exdir/blob/89c1d34a5ce65fefc09b6fe1c5e8fef68c494e75/exdir/core/exdir_object.py#L128
-    path = realpath(path)
-    found = false
-    while !found
-        (parent, pname) = splitdir(path)
-        if parent == path
-            return nothing
-        end
-        if !is_nonraw_object_directory(path)
-            path = parent
-            continue
-        end
-        meta_data = YAML.load_file(joinpath(path, META_FILENAME))
-        if !haskey(meta_data, EXDIR_METANAME)
-            path = parent
-            continue
-        end
-        exdir_meta = meta_data[EXDIR_METANAME]
-        if !haskey(exdir_meta, TYPE_METANAME)
-            path = parent
-            continue
-        end
-        if FILE_TYPENAME != exdir_meta[TYPE_METANAME]
-            path = parent
-            continue
-        end
-        found = true
-    end
-    path
-end
-
-function is_inside_exdir(path::AbstractString)
-    # https://github.com/CINPLA/exdir/blob/89c1d34a5ce65fefc09b6fe1c5e8fef68c494e75/exdir/core/exdir_object.py#L161
-    path = realpath(path)
-    !isnothing(root_directory(path))
-end
-
-function assert_inside_exdir(path::AbstractString)
-    # https://github.com/CINPLA/exdir/blob/89c1d34a5ce65fefc09b6fe1c5e8fef68c494e75/exdir/core/exdir_object.py#L166
-    if !is_inside_exdir(path)
-        error("Path " + path + " is not inside an Exdir repository.")
-    end
 end
 
 function open_object(directory::AbstractString)
